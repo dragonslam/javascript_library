26bf72755e2ac87e49548169b1c8daf24d02758e
Yi, Seung Yong <dragonslam@gmail.com>
2022-09-27 13:49:28 +0900

    Update biz.base.js

--- commonES6/biz/biz.base.js
+++ commonES6/biz/biz.base.js
@@ -66,7 +66,7 @@
 		This.initEventListner();
 
 		
-		Base.logging(This, 'initialize completed.');010
+		Base.logging(This, 'initialize completed.');
 		return This;
 	};
 	Module.initEventListner= function() {
